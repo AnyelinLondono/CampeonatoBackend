45aa5c26837a4beaf029942d90ae4d5ce7e12d61
AnyelinLondono <149441669+AnyelinLondono@users.noreply.github.com>
2024-06-02 00:44:57 -0500

    agrega equipo repositorio

---
+++ src/main/java/com/ucentral/CampeonatoProy/repository/EquiposRepository.java
@@ -0,0 +1,7 @@
+package com.ucentral.CampeonatoProy.repository;
+
+import com.ucentral.CampeonatoProy.entity.Equipos;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EquiposRepository extends CrudRepository<Equipos, Integer> {
+}
